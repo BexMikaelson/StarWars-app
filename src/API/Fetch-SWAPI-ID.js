18fb9456d1cd88ec18014dbbd8b5bc27ec1d5670
Heidi sjöberg <heidi.alexx8@gmail.com>
2022-09-29 18:15:29 +0200

    fetching id from swapi api to disply film info for each film

---
+++ src/API/Fetch-SWAPI-ID.js
@@ -0,0 +1,11 @@
+export const getSwapiIdFromUrl = (url) => {
+	// eslint-disable-next-line no-unused-vars
+	const [_endpoint, id] = url
+		.replace('https://swapi.dev/api/', '')
+		.slice(0, -1)
+		.split('/')
+
+	return id
+}
+
+export default getSwapiIdFromUrl
