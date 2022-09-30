c36bbaba43ee2989062f3dd96c6316fc9832d7ad
Heidi sjöberg <heidi.alexx8@gmail.com>
2022-09-30 13:51:49 +0200

    adding img to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,22 @@
+import wisdom_yoda from "../assets/images/wisdom_yoda.jpeg";
 const Home = () => {
-    return ( <div>
-        Home
-    </div>  );
-}
- 
-export default Home; <div>
-Home</div>
+  return (
+    <div>
+      <h1>Welcome to all things Star Wars!</h1>
+      <h4>But in mind you must keep!</h4>
+      <h4>
+        The FORSE is NOT a POWER you have, it's NOT about lifting ROCKS.
+        <br />
+        It's about the ENERGY between all THINGS, a balance, that BINDS the
+        UNIVERSE together.
+      </h4>
+      <p>-Luke Skywalker</p>
+
+      <div className="image">
+        <img src={wisdom_yoda} alt="yoda" className="img-fluid" />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
