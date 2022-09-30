4dd9e04746688efe67ac73ef07870bb010a88d29
Heidi sjöberg <heidi.alexx8@gmail.com>
2022-09-30 13:52:11 +0200

    cleaning upp

--- src/pages/People.jsx
+++ src/pages/People.jsx
@@ -1,119 +1,109 @@
 // API
-import SWAPI from "../API/SWAPI"
-import getSwapiIdFromUrl from '../API/Fetch-SWAPI-ID'
+import SWAPI from "../API/SWAPI";
+import getSwapiIdFromUrl from "../API/Fetch-SWAPI-ID";
 
 // Imported react
-import { useEffect, useState } from "react"
-import { Link, useParams, useNavigate } from "react-router-dom"
-import ListGroup from "react-bootstrap/ListGroup"
+import { useEffect, useState } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import ListGroup from "react-bootstrap/ListGroup";
 
 //Loading
-import LoadingSpinner from '../components/LoadingSpinner'
-
+import LoadingSpinner from "../components/LoadingSpinner";
 
 const PeoplePage = () => {
-    const [person, setPerson] = useState()
-    const [loading, setLoading] = useState(false)
-    const { id } = useParams()
-    const navigate = useNavigate()
+  const [person, setPerson] = useState();
+  const [loading, setLoading] = useState(false);
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-    // Get person from API
-	const fetchPerson = async (id) => {
-        // get data
-		const data = await SWAPI.getPeople(id)
+  // Get person from API
+  const fetchPerson = async (id) => {
+    // get data
+    const data = await SWAPI.getPeople(id);
 
-        // set data results to person
-		setPerson(data)
+    // set data results to person
+    setPerson(data);
 
-        
-        setLoading(true)
-	}
+    setLoading(true);
+  };
 
-    // Get people from api when component is first mounted
-	useEffect(() => {
-		fetchPerson(id)
-	}, [id])
+  // Get people from api when component is first mounted
+  useEffect(() => {
+    fetchPerson(id);
+  }, [id]);
 
-    if(!loading) {
-        return (
-            <LoadingSpinner />
-        )
-    }
+  if (!loading) {
+    return <LoadingSpinner />;
+  }
 
-    return (
-        <>
-            <ListGroup className="card mb-5 p-0">
-                <div className="card-header p-3">
-                    <h4>{person.name}</h4>
-                </div>
+  return (
+    <>
+      <ListGroup className="card mb-5 p-0">
+        <div className="card-header p-3">
+          <h4>{person.name}</h4>
+        </div>
 
-                <div className="card-body">
+        <div className="card-body">
+          <div>
+            <h5>Gender: </h5>
+            <p>{person.gender}</p>
+          </div>
 
-                    <div>
-                        <h5>Gender: </h5>
-                        <p>{person.gender}</p>
-                    </div>
+          <div>
+            <h5>Birth year: </h5>
+            <p>{person.birth_year}</p>
+          </div>
 
-                    <div>
-                        <h5>Birth year: </h5>
-                        <p>{person.birth_year}</p>
-                    </div>
+          <div>
+            <h5>Height: </h5>
+            <p>{person.height} cm</p>
+          </div>
 
-                    <div>
-                        <h5>Height: </h5>
-                        <p>{person.height} cm</p>
-                    </div>
+          <div>
+            <h5>Mass: </h5>
+            <p>{person.mass} kg</p>
+          </div>
 
-                    <div>
-                        <h5>Mass: </h5>
-                        <p>{person.mass} kg</p>
-                    </div>
+          <div>
+            <h5>Hair color: </h5>
+            <p>{person.hair_color}</p>
+          </div>
 
-                    <div>
-                        <h5>Hair color: </h5>
-                        <p>{person.hair_color}</p>
-                    </div>
+          <div>
+            <h5>Skin color: </h5>
+            <p>{person.skin_color}</p>
+          </div>
 
-                    <div>
-                        <h5>Skin color: </h5>
-                        <p>{person.skin_color}</p>
-                    </div>
+          <div>
+            <h5>Eye color: </h5>
+            <p>{person.eye_color}</p>
+          </div>
 
-                    <div>
-                        <h5>Eye color: </h5>
-                        <p>{person.eye_color}</p>
-                    </div>
+          <br />
 
-                    <br />
+          <ListGroup>
+            <h4>{person.name} in these films you see</h4>
 
-                    <ListGroup>
-                        
-                        <h4>{person.name} in these films you see</h4>
-                        
-                        {person.films.length > 0 && (
-                            person.films.map((film, index) => 
-                            <Link 
-                                key={index} 
-                                to={`/films/${getSwapiIdFromUrl(film)}`} 
-                                className="list-group-item col-md-4" 
-                            >
-                                Film {getSwapiIdFromUrl(film)}
-                            </Link>
-                        ))}
-                    </ListGroup>
-                </div>
-                <div className="buttons d-flex justify-content-between p-3">
-                    <button
-                            type="button" 
-                            onClick={() => navigate(-1)} 
-                            className="btn" 
-                        >
-                            Back
-                    </button>
-                </div>
-            </ListGroup>
-        </>
-    )
-}
+            {person.films.length > 0 &&
+              person.films.map((film, index) => (
+                <Link
+                  key={index}
+                  to={`/films/${getSwapiIdFromUrl(film)}`}
+                  className="list-group-item col-md-4"
+                >
+                  Film {getSwapiIdFromUrl(film)}
+                </Link>
+              ))}
+          </ListGroup>
+        </div>
+        <div className="buttons d-flex justify-content-between p-3">
+          <button type="button" onClick={() => navigate(-1)} className="btn">
+            Back
+          </button>
+        </div>
+      </ListGroup>
+    </>
+  );
+};
 
-export default PeoplePage
+export default PeoplePage;
--- src/pages/PeoplePage.jsx
+++ src/pages/PeoplePage.jsx
@@ -1,115 +1,105 @@
 // API
-import SWAPI from "../API/SWAPI"
+import SWAPI from "../API/SWAPI";
 
 // Imported react
-import { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
-import ListGroup from "react-bootstrap/ListGroup"
-import Button from 'react-bootstrap/Button'
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import ListGroup from "react-bootstrap/ListGroup";
+import Button from "react-bootstrap/Button";
 
 // Imported Loading Spinner
-import LoadingSpinner from '../components/LoadingSpinner'
-
+import LoadingSpinner from "../components/LoadingSpinner";
 
 const PeoplesPage = () => {
-    const [people, setPeople] = useState([])
-    const [loading, setLoading] = useState(false)
-    const [page, setPage] = useState(1)
-    //const [peoplePerPage, setPeoplePerPage] = useState(4)
+  const [people, setPeople] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  
 
-	// Get people from api when component is first mounted
-	useEffect(() => {
-        
-        // Get people from API
-        const fetchPeople = async () => {
-            
-            setLoading(true)
+  // Get people from api when component is first mounted
+  useEffect(() => {
+    // Get people from API
+    const fetchPeople = async () => {
+      setLoading(true);
 
-            // get data
-            const data = await SWAPI.getPeoples(page)
+      // get data
+      const data = await SWAPI.getPeoples(page);
 
-            // set data results to people
-            setPeople(data)
+      // set data results to people
+      setPeople(data);
 
-            // Set loading to false after getting data.
-            setLoading(false)
-        }
+      // Set loading to false after getting data.
+      setLoading(false);
+    };
 
-		fetchPeople()
-	}, [page]);
+    fetchPeople();
+  }, [page]);
 
-    
-    return (
-        <>
-        
-            {loading && <LoadingSpinner />}
+  return (
+    <>
+      {loading && <LoadingSpinner />}
 
-            <h1>People</h1>
-            
+      <h1>People</h1>
 
-            {!loading && (
-                <div className="row d-flex justify-content-between">
-                    {people.results?.map((people, index) => (
-                        <ListGroup.Item 
-                            key={index} 
-                            className="card border-1 rounded m-3 div-0 col-lg-3 col-md-5 col-sm-12"
-                        >
-                            <div className="card-header p-3">
-                                <h4>{people.name}</h4>
-                            </div>
+      {!loading && (
+        <div className="row d-flex justify-content-between">
+          {people.results?.map((people, index) => (
+            <ListGroup.Item
+              key={index}
+              className="card border-1 rounded m-3 div-0 col-lg-3 col-md-5 col-sm-12"
+            >
+              <div className="card-header p-3">
+                <h4>{people.name}</h4>
+              </div>
 
-                            <div className="card-body">
-                                <div className="card-header bg-white">
-                                    <h6>Gender: </h6>
-                                    {people.gender}
-                                </div>
-                                <div className="card-header bg-white">
-                                    <h6>Born: </h6>
-                                    {people.birth_year}
-                                </div>
-                                <div className="card-header bg-white mb-3">
-                                    <h6>In </h6>
-                                    {people.films.length} films
-                                </div>
-                                
-                                <Link 
-                                    type="button" 
-                                    to={`/people/${index + 1}`} 
-                                    className="btn btn-primary" 
-                                >
-                                    Read More
-                                </Link>
-                            </div>
-                        </ListGroup.Item>
-                    ))}
+              <div className="card-body">
+                <div className="card-header bg-white">
+                  <h6>Gender: </h6>
+                  {people.gender}
                 </div>
-            )}
+                <div className="card-header bg-white">
+                  <h6>Born: </h6>
+                  {people.birth_year}
+                </div>
+                <div className="card-header bg-white mb-3">
+                  <h6>In </h6>
+                  {people.films.length} films
+                </div>
 
-
-
-            <div className="d-flex justify-content-between mt-4">
-                <Button
-                    variant="button" 
-                    disabled={page === 1} 
-                    className="btn btn-primary border-secondary"
-                    onClick={() => setPage(prevValue => prevValue - 1)}
+                <Link
+                  type="button"
+                  to={`/people/${index + 1}`}
+                  className="btn btn-primary"
                 >
-                    Previous Page
-                </Button>
+                  Read More
+                </Link>
+              </div>
+            </ListGroup.Item>
+          ))}
+        </div>
+      )}
 
-                {page} / 9
+      <div className="d-flex justify-content-between mt-4">
+        <Button
+          variant="button"
+          disabled={page === 1}
+          className="btn btn-primary border-secondary"
+          onClick={() => setPage((prevValue) => prevValue - 1)}
+        >
+          Previous Page
+        </Button>
+        {page} / 9
+        <Button
+          variant="button"
+          disabled={!people.next}
+          className="btn btn-primary border-secondary"
+          onClick={() => setPage((prevValue) => prevValue + 1)}
+        >
+          Next Page
+        </Button>
+      </div>
+    </>
+  );
+};
 
-                <Button
-                    variant="button" 
-                    disabled={!people.next} 
-                    className="btn btn-primary border-secondary" 
-                    onClick={() => setPage(prevValue => prevValue + 1)}
-                >
-                    Next Page
-                </Button>
-                    </div>
-        </>
-	)
-}
-
-export default PeoplesPage
+export default PeoplesPage;
